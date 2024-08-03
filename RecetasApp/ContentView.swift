59bea5da5c5d2c1ca169aa57b8af48bd7e63d168
Ronado Vargas <ronaldovh99@gmail.com>
2024-08-03 12:29:00 -0500

    Initial Commit

---
+++ RecetasApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  RecetasApp
+//
+//  Created by Ronaldo Andre on 3/08/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ RecetasApp/RecetasAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecetasAppApp.swift
+//  RecetasApp
+//
+//  Created by Ronaldo Andre on 3/08/24.
+//
+
+import SwiftUI
+
+@main
+struct RecetasAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
