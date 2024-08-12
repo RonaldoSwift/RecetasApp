63d0f47b4a6fc635f115df1083e51213d6f9e41f
Ronado Vargas <ronaldovh99@gmail.com>
2024-08-12 10:27:32 -0500

    Add MapScreen, Modelo Receta, Marcador De Restaurante.

--- RecetasApp/Presentation/Map/MapScreenView.swift
+++ RecetasApp/Presentation/Map/MapScreenView.swift
@@ -6,17 +6,44 @@
 //
 
 import SwiftUI
+import MapKit
 
 struct MapScreenView: View {
     
+    @State private var region = MKCoordinateRegion(
+        center: CLLocationCoordinate2D(latitude: -12.0464, longitude: -77.0428),
+        span: MKCoordinateSpan(latitudeDelta: 0.05, longitudeDelta: 0.05)
+    )
+    
+    let marcadores = [RestauranteMarcador(coordenadas: CLLocationCoordinate2D(latitude: -12.0464, longitude: -77.0428), titulo: "Gusto"),
+                      RestauranteMarcador(coordenadas: CLLocationCoordinate2D(latitude: -12.0480, longitude: -77.0480), titulo: "Dulce")
+    ]
+    
     var body: some View {
         VStack {
-            Text("Map")
+            Map(coordinateRegion: $region, annotationItems: marcadores) { marcador in
+                MapAnnotation(coordinate: marcador.coordenadas) {
+                    Button {
+                        print("Mapa")
+                    } label: {
+                        VStack {
+                            Image(systemName: "mappin.circle.fill")
+                                .resizable()
+                                .frame(width: 30, height: 30)
+                                .foregroundColor(.red)
+                            Text(marcador.titulo)
+                                .font(.caption)
+                                .background(Color.white)
+                        }
+                    }
+                }
+            }
         }
         .toolbar(content: {
             TextToolbarContent(tituloDePantalla: "Mapa")
         })
         .navigationBarBackButtonHidden(true)
+        .ignoresSafeArea(.all)
     }
 }
 
